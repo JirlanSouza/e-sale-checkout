a2ea3b8768061f7e3a6ecdac1733d8ebd9141c4c
Jirlan Souza <jirlansouza08@gmail.com>
2022-12-17 16:12:48 -0400

    refactor: change the Swagger description

--- src/main.ts
+++ src/main.ts
@@ -4,26 +4,26 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function init() {
-    const app = await NestFactory.create(AppModule);
-    app.connectMicroservice({
-        transport: Transport.RMQ,
-        options: {
-            url: "amqp://localhost",
-            queue: "placeOrder",
-        },
-    });
+  const app = await NestFactory.create(AppModule);
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      url: "amqp://localhost",
+      queue: "placeOrder",
+    },
+  });
 
-    const config = new DocumentBuilder()
-        .setTitle("E-sale checkout micro-service")
-        .setDescription("The checkout couded context api")
-        .setVersion("1.0")
-        .addTag("checkout")
-        .build();
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup("doc", app, document);
+  const config = new DocumentBuilder()
+    .setTitle("E-sale-checkout micro-service")
+    .setDescription("The checkout microservice off E-sale system")
+    .setVersion("1.0")
+    .addTag("checkout")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("doc", app, document);
 
-    const port = parseInt(process.env.PORT) || 3000;
-    await app.startAllMicroservices();
-    await app.listen(port);
+  const port = parseInt(process.env.PORT) || 3000;
+  await app.startAllMicroservices();
+  await app.listen(port);
 }
 init();
