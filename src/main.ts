d5dfbf18703b5afdde020f8f6e5f8380dc879290
Jirlan Souza <jirlansouza08@gmail.com>
2022-12-18 21:21:46 -0400

    refactor: change the swagger and microservices setup to use the owners setup class

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,14 @@
 import { NestFactory } from "@nestjs/core";
-import { Transport } from "@nestjs/microservices";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
+import { Swagger } from "./infra/documentation/swagger/Swagger";
+import { QueueMicroservices } from "./infra/queue/QueueMicroservice";
 
 async function init() {
-  const app = await NestFactory.create(AppModule);
-  app.connectMicroservice({
-    transport: Transport.RMQ,
-    options: {
-      url: "amqp://localhost",
-      queue: "placeOrder",
-    },
-  });
-
-  const config = new DocumentBuilder()
-    .setTitle("E-sale-checkout micro-service")
-    .setDescription("The checkout microservice off E-sale system")
-    .setVersion("1.0")
-    .addTag("checkout")
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("doc", app, document);
-
-  const port = parseInt(process.env.PORT) || 3000;
-  await app.startAllMicroservices();
-  await app.listen(port);
+    const app = await NestFactory.create(AppModule);
+    QueueMicroservices.setup(app);
+    Swagger.setup(app);
+    const port = parseInt(process.env.PORT) || 3000;
+    await app.startAllMicroservices();
+    await app.listen(port);
 }
 init();
